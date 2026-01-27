6bb4801226561a52d39fed93f9c9364959c29337
Akash-Veerla <akashveerla0@gmail.com>
2026-01-27 18:14:01 +0530

    Fix: Add missing http and socket.io imports

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
+const socketIo = require('socket.io');
 const fs = require('fs');
 const path = require('path');
 const csv = require('csv-parser');
